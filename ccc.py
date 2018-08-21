e5608537859e83cb032fa7d3b59226c8d9dfa548
SergeiShumilin <NoiSD@yandex.ru>
2018-08-21 18:46:44 +0300

    /

---
+++ ccc.py
@@ -0,0 +1,19 @@
+import numpy as np
+from numpy.core.tests.test_mem_overlap import xrange
+import numpy.random as rd
+from collections import OrderedDict
+from timeit import default_timer as timer
+import time
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+
+mu, sigma = 10, 15
+x = mu + sigma*np.random.randn(10000)
+
+
+n, bins, patches = plt.hist(x, 500, normed=1, facecolor='green', alpha=0.75)
+y = mlab.normpdf( bins, mu, sigma)
+l = plt.plot(bins, y, 'r--', linewidth=1)
+
+plt.show()
--- shell_sort_analysis.py
+++ shell_sort_analysis.py
@@ -1,8 +1,9 @@
 from numpy import *
 from numpy.core.tests.test_mem_overlap import xrange
 import numpy.random as rd
+from collections import OrderedDict
 from timeit import default_timer as timer
-
+import time
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -18,29 +19,47 @@ def shellsort(a):
                 i = int(round(i / 2.2))
             yield i
 
-    dict = {}
+    list_of_dict = []
+
     for increment in new_increment(a):
-        i = 0
+        dict = {}
         for i in xrange(increment, len(a)):
+            t = 0
             for j in xrange(i, increment - 1, -increment):
                 if a[j - increment] < a[j]:
                     break
                 a[j], a[j - increment] = a[j - increment], a[j]
-                i+=i
-        dict.update({increment: i})
-    print(dict)
-    return dict
+                t+=1
+            v = dict.get(t)
+            if v==None:
+                v = 0
+            dict.update({t: v+1})
+        dict = sorted(dict.items(), key=lambda t: t[0])
+        list_of_dict.append(dict)
+        get_splot(list_of_dict)
+
+    plt.show()
 
 
 def get_array(size):
     return rd.sample(size)
 
-def print_change_freq(array):
+def print_hist(array):
+    shellsort(array)
 
-    shellsort(array).plt.hist(grid=True, bins=20, rwidth=0.9,
-                   color='#607c8e')
-    plt.show()
+def get_splot(dict_list):
+    fig, axs = plt.subplots(2,1)
+    i = 0
+    for ax in fig.axes:
+        keys, values = zip(*dict_list[i])
+        i += 1
+        ax.bar(keys, values, width=0.5)
 
 
 
-print_change_freq(get_array(100))
+
+
+
+
+
+print_hist(get_array(100))
