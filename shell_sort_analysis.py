9ed22e8843ae8a70fa258ac35aaacc5e743d2c29
SergeiShumilin <NoiSD@yandex.ru>
2018-08-28 19:09:02 +0300

    /

--- shell_sort_analysis.py
+++ shell_sort_analysis.py
@@ -4,31 +4,56 @@ import numpy.random as rd
 import matplotlib.pyplot as plt
 
 
-def shellsort(a, v, Show_dict=False, Show_Maximized_Dict=False, Show_Itarations=False, Show_Hist = False):
-    def new_increment2(a):
-        N = int(len(a))
+def shellsort(a, v, seq, Show_dict=False, Show_Maximized_Dict=False, Show_Itarations=False, Show_Hist=False):
+    def pratt2(array):
+        res = []
+        N = int(len(array))
         h = 1
         while h < N / 3:
             h = 3 * h + 1
         while h >= 1:
-            yield h
+            res.append(h)
             h //= 3
+        return res
+
+    def pratt1(array):
+        """Generate a sorted list of products of powers of 2 and 3 below max_size"""
+        products = []
+        max_size = len(array) // 2
+        pow3 = 1  # start with q = 0
+        while pow3 <= max_size:
+            # At this point, pow3 = 3**q, so set p = 0
+            pow2 = pow3
+            while pow2 <= max_size:
+                # At this point, pow2 = 2**p * 3**q
+                products.append(pow2)
+                pow2 = pow2 * 2  # this is like adding 1 to p
+            # now that p overflowed the maximum size, add 1 to q and start over
+            pow3 = pow3 * 3
+        return list(reversed(sorted(products)))
 
     dict = {}
     ks = []
     list_of_dicts = []
-    for increment in new_increment2(a):
+    gaps = []
+
+    if seq == 1:
+        gaps = pratt1(a)
+    else:
+        gaps = pratt2(a)
+
+    for gap in gaps:
         values = []
-        for i in xrange(increment, len(a)):
+        for i in xrange(gap, len(a)):
             iter = 0
-            for j in xrange(i, increment - 1, -increment):
+            for j in xrange(i, gap - 1, -gap):
                 iter += 1
-                if a[j - increment] < a[j]:
+                if a[j - gap] < a[j]:
                     break
-                a[j], a[j - increment] = a[j - increment], a[j]
+                a[j], a[j - gap] = a[j - gap], a[j]
             values.append(iter)
-        ks.append(increment)
-        dict.update({increment: values})
+        ks.append(gap)
+        dict.update({gap: values})
     list_of_dicts.append(dict)
 
     max_dic = maximize(dict, v)
@@ -44,11 +69,11 @@ def shellsort(a, v, Show_dict=False, Show_Maximized_Dict=False, Show_Itarations=
         print('Кол-во итераций внутр. цикла в векторизованном коде: Ivect = ' + str(Ivect))
         print('Отношение числа итераций Iorigin/Ivect = ' + str(I))
 
-
     if Show_Hist is True:
         get_splot(max_dic, ks)
 
     return I
+
 
 def get_array(size):
     return rd.sample(size)
@@ -94,12 +119,19 @@ def print_maxim_dict(max_dict):
     for key, value in max_dict.items():
         print('k = ' + str(key) + ': ' + str(value))
 
+
 def number_of_el_I_depend():
-    a = [10**i for i in range(1000,1000000,5000)]
-    b = [shellsort(get_array(size),16,Show_Itarations=True) for size in a]
-#    plt.scatter(a, b)
-    plt.plot(a,b, '-o')
+    x = [i for i in range(10, 1000000, 50000)]
+    y = [shellsort(get_array(size), 16, Show_Itarations=True) for size in x]
+    print(x)
+    print(y)
+    a, b, c = polyfit(x, y, 2)
+    x_out = linspace(0, max(x), 1000)  # choose 20 points, 10 in, 10 outside original range
+    y_pred = polyval([a, b, c], x_out)
+    #    plt.scatter(a, b)
+    plt.plot(x, y, '-o')
+    plt.plot(x, y, 'g.', x_out, y_pred, 'b-')
     plt.show()
 
 
-number_of_el_I_depend()
+shellsort(get_array(1000), 16, 1, Show_Hist=True)
