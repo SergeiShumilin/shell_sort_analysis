0e4caba41894c9019a230629d638973caaa03a48
SergeiShumilin <NoiSD@yandex.ru>
2018-08-31 18:31:09 +0300

    labels for each line added

--- speedup_graphs.py
+++ speedup_graphs.py
@@ -15,7 +15,7 @@ import matplotlib.pyplot as plt
 from os import listdir
 from os.path import isfile, join
 import seaborn as sb
-sb.set()
+import matplotlib.ticker as tick
 
 def collect_names(how='', path_to_dir='C:\\Users\\Sergei\\Desktop\\data'):
     onlyfiles = [f for f in listdir(path_to_dir) if isfile(join(path_to_dir, f))]
@@ -66,18 +66,49 @@ def plot_together(list_of_files):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
-    ax.set_title('Comparing of sequences for Shell Sort')
-    ax.axes.set_xlabel('Number of elements')
-    ax.axes.set_ylabel('Vectorization efficiency')
+    add_inscriptions(ax)
+
 
     set_limits(ax,list_of_files)
 
     for file in list_of_files:
         el_num, eff_coef = get_data(file)
-        plt.plot(el_num, eff_coef, label=file)
+        if file.find('_2') != -1:
+            if file.find('without') != -1:
+                plt.plot(el_num, eff_coef, label='N/2 without k=1', color='#FF3D00', linestyle='--')
+            else:
+                plt.plot(el_num, eff_coef, label='N/2', color='#FF3D00')
+        if file.find('_s') != -1:
+            if file.find('without') != -1:
+                plt.plot(el_num, eff_coef, label="Sedgewick without k=1", color='#FFB600', linestyle='--')
+            else:
+                plt.plot(el_num, eff_coef, label="Sedgewick", color='#FFB600')
+        if file.find('_f') != -1:
+            if file.find('without') != -1:
+                plt.plot(el_num, eff_coef, label="Febonacci without k=1", color='#162EAE', linestyle='--')
+            else:
+                plt.plot(el_num, eff_coef, label="Febonacci", color='#162EAE')
+        if file.find('_p') != -1:
+            if file.find('without') != -1:
+                plt.plot(el_num, eff_coef, label='Pratt without k=1', color='#00B358', linestyle='--')
+            else:
+                plt.plot(el_num, eff_coef, label='Pratt', color='#00B358')
+        if file.find('_h') != -1:
+            if file.find('without') != -1:
+                plt.plot(el_num, eff_coef, label='Hibbard without k=1', color='#ff66ff', linestyle='--')
+            else:
+                plt.plot(el_num, eff_coef, label='Hibbard', color='#ff66ff')
+
 
     plt.legend(loc='upper right')
     plt.show()
+
+
+def add_inscriptions(ax):
+    ax.set_title('Comparing of sequences for Shell Sort',size = 15,weight=700)
+    ax.axes.set_xlabel('Number of elements',size = 12)
+    ax.axes.set_ylabel('Vectorization efficiency',size = 12)
+
 
 def set_limits(ax,list_of_files):
     if list_of_files[0].find('without_1') != -1:
@@ -87,23 +118,9 @@ def set_limits(ax,list_of_files):
 
 def plot_with_params(file_name, subplot, elem_num, effic_coef):
     subplot.set_title(file_name)
-    subplot.axes.set_xlabel('Number of elements')
+    subplot.axes.set_xlabel('Number of elements',size = 15)
     subplot.axes.set_ylabel('Vectorization efficiency')
     subplot.set_ylim(0, 3)
     subplot.plot(elem_num, effic_coef)
 
-
-def plot_it(elements_number, efficiency_coef, filename):
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.set_title(filename)
-    ax.axes.set_xlabel('Number of elements')
-    ax.axes.set_ylabel('Vectorization efficiency')
-    ax.set_ylim(0, 3)
-    plt.plot(elements_number, efficiency_coef)
-    plt.show()
-
-
-plot_together(collect_names())
-plot_together(collect_names('without1'))
 plot_together(collect_names('all'))
